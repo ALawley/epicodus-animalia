7d4ad2a7240fdcf74ac882c9af8d01dc39b80a27
Anna Lawley and paul Hess <alawley1@gmail.com>
2016-01-12 09:22:31 -0800

    add buttons to add additional images.

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,18 @@ $(document).ready(function() {
     $("ul#dogsays").append("<li>Bark!</li>");
     $("ul#catsays").append("<li>Meow!</li>");
   });
+  $("button#puppyshow").click(function() {
+    $("button#puppyshow").after("<img class='hidden-puppy'src='img/puppy3.jpg'>");
+
+    $("img.hidden-puppy").first().click(function() {
+      $(this).remove();
+    });
+  });
+  $("button#kittyshow").click(function() {
+    $("button#kittyshow").after("<img class='hidden-kitty'src='img/kitty.jpg'>");
+
+    $("img.hidden-kitty").first().click(function() {
+      $(this).remove();
+    });
+  });
 });
